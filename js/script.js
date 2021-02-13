22c200953047fe86b367595649c404c1f6997649
mishazverev <misha.zverev@gmail.com>
2021-02-13 18:19:33 +0300

    1

--- js/script.js
+++ js/script.js
@@ -224,36 +224,32 @@ window.addEventListener('DOMContentLoaded', () => {
             statusMessage.style.cssText = `
                 display: block;
                 margin: 0 auto;
-                
+
             `;
-            form.append(statusMessage);
-
-            const request = new XMLHttpRequest();
-            request.open('POST', 'server.php');
-            request.setRequestHeader('Content-type', 'application/json');
-
+            form.append(statusMessage);      
             const formData = new FormData(form);
-            const object = {};
-            formData.forEach(function(value, key){
-                object[key] = value;                
-            });
 
-            const json = JSON.stringify(object);
+            // request.setRequestHeader('Content-type', 'application/json');
+            // const object = {};
+            // formData.forEach(function(value, key){
+            //     object[key] = value;                
+            // });
+            // const json = JSON.stringify(object);
 
-
-            request.send(json);
-
-            request.addEventListener('load', () => {
-                if (request.status === 200){
-                    console.log(request.response);
+            fetch('server.php',{
+                method: "POST",
+                // headers: {
+                //     'Content-type': 'application/json'
+                // },
+                body: formData
+            }).then(data => {
+                    console.log(data);
                     showThanksModal(message.success);
-                    form.reset();                    
                     statusMessage.remove();
-                    
-                    
-                } else {
+            }).catch(() => {
                     showThanksModal(message.failure);
-                }
+            }).finally(() => {
+                    form.reset();              
             });
 
         });
@@ -281,6 +277,5 @@ window.addEventListener('DOMContentLoaded', () => {
             closeModal();
         }, 3000);
     }
-
-    
+ 
 });
