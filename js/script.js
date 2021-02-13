3fdbc3026dc594e6ac790ee4198e10fdae4fa679
mishazverev <misha.zverev@gmail.com>
2021-02-13 15:57:07 +0300

    main

--- js/script.js
+++ js/script.js
@@ -235,6 +235,11 @@ window.addEventListener('DOMContentLoaded', () => {
                 if (request.status === 200){
                     console.log(request.response);
                     statusMessage.textContent = message.success;
+                    form.reset();
+                    setTimeout(() => {
+                        statusMessage.remove();
+                     }, 2000);
+                    
                 } else {
                     statusMessage.textContent = message.failure;
                 }
