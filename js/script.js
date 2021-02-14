257364fa196ea50d19e12bc45cf1f3211e18dae1
mishazverev <misha.zverev@gmail.com>
2021-02-14 13:59:15 +0300

    1

--- js/script.js
+++ js/script.js
@@ -171,36 +171,20 @@ window.addEventListener('DOMContentLoaded', () => {
             this.parent.append(element);
         }
     }
-    new MenuCard(
-        "img/tabs/vegy.jpg",
-        "vegy",
-        'Меню "Фитнес"',
-        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-        10,
-        '.menu .container',
+    const getResource = async (url) => {
+        const res = await fetch(url);
+        if (!res.ok){
+           throw new Error(`Could not fetch ${url}, status ${res.status}`);
+        }
+        return await res.json();
+    };
+    getResource('http://my-json-server.typicode.com/mishazverev/study1/menu')
+        .then (data => {
+            data.forEach(({img, altimg, title, descr, price}) => {
+                new MenuCard.render(img, altimg, title, descr, price);
+            });
+        });
     
-    ).render();
-
-    new MenuCard(
-        "img/tabs/elite.jpg",
-        "elite",
-        'Меню “Премиум”',
-        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-        12,
-        '.menu .container',
-        'menu__item'
-    ).render();
-
-    new MenuCard(
-        "img/tabs/post.jpg",
-        "post",
-        'Меню "Постное"',
-        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
-        15,
-        '.menu .container',
-        'menu__item'
-    ).render();
-
     // Forms
     const forms = document.querySelectorAll ("form");
 
