b916c1d4cb0428eb0186ab2ab7dcf1f5a0c7e4be
mishazverev <misha.zverev@gmail.com>
2021-02-14 17:38:12 +0300

    1

--- js/script.js
+++ js/script.js
@@ -274,11 +274,19 @@ window.addEventListener('DOMContentLoaded', () => {
 
     const slides = document.querySelectorAll('.offer__slide'),
             prev = document.querySelector('.offer__slider-prev'),
-            next = document.querySelector('.offer__slider-next');
+            next = document.querySelector('.offer__slider-next'),
+            total = document.querySelector('#total'),
+            current = document.querySelector('#current');
     
     let slideIndex = 1;
 
     showSlides(slideIndex);
+
+    if (slides.length < 10) {
+        total.textContent = `0${slides.length}`;
+    } else {
+        total.textContent = slides.length;
+    }
 
     function showSlides(n) {
         if (n > slides.length) {
@@ -290,7 +298,13 @@ window.addEventListener('DOMContentLoaded', () => {
         }
 
         slides.forEach(item => item.style.display = 'none');
-        slides[slideIndex - 1].style.display = 'block';        
+        slides[slideIndex - 1].style.display = 'block'; 
+        
+        if (slides.length < 10) {
+            current.textContent = `0${slideIndex}`;
+        } else {
+            current.textContent = slideIndex;
+        }
     }
 
     function plusSlides(n) {
